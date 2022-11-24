a84a0503e7843e135f20c95e90afb0b88cc94ec2
Carolina Hangai <carolina.hangai@gmail.com>
2022-11-24 13:52:04 -0300

    Correção

--- Exercicio034.py
+++ Exercicio034.py
@@ -2,7 +2,17 @@
 Para salários superiores a R$1250,00, calcule um aumento de 10%.
 Para os inferiores ou iguais, o aumento é de 15%."""
 
-sal = float(input('Qual o seu salario?:'))
+"""sal = float(input('Qual salario do funcionario?'))
 if sal > 1250:
-    print(sal)*0.10
-    
+    aumento = (sal*0.10)
+    print('Quem ganhava R$',sal, 'passa a ganhar R$',aumento + sal, 'agora.')
+if sal <= 1250:
+    aumento = (sal*0.15)
+    print('Quem gannhava R$', sal, 'passa a ganhar R$', aumento + sal, 'agora.')"""
+
+sal = float(input('Qual salario do funcionario?'))
+if sal <=1250:
+    aumento = (sal + 1250 *15/100)
+else:
+    aumento = (sal + 1250 * 10/100)
+print('Quem ganhava R${:.2f} passa a ganhar R${:.2f} agora.'.format(sal, aumento))
